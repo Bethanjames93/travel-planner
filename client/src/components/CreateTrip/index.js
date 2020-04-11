6eba8c71f5da010107fbf8466e80f9b653e86a59
Tom Buckley <thomasbuckley@gmail.com>
2020-04-11 12:16:21 +0100

    Update axios url (test)

--- client/src/components/CreateTrip/index.js
+++ client/src/components/CreateTrip/index.js
@@ -1,133 +1,146 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateTrip extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onChangeLocation = this.onChangeLocation.bind(this);
-        this.onChangeLandmark = this.onChangeLandmark.bind(this);
-        this.onChangeViewpoint = this.onChangeViewpoint.bind(this);
-        this.onChangePhotospot = this.onChangePhotospot.bind(this);
-        this.onChangeCafe = this.onChangeCafe.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeLocation = this.onChangeLocation.bind(this);
+    this.onChangeLandmark = this.onChangeLandmark.bind(this);
+    this.onChangeViewpoint = this.onChangeViewpoint.bind(this);
+    this.onChangePhotospot = this.onChangePhotospot.bind(this);
+    this.onChangeCafe = this.onChangeCafe.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-        this.state = {
-            location: '',
-            landmark: '',
-            viewpoint: '',
-            photospot: '',
-            cafe: '',
-        }
-    }
+    this.state = {
+      location: "",
+      landmark: "",
+      viewpoint: "",
+      photospot: "",
+      cafe: "",
+    };
+  }
 
-    // componentDidMount() {
-    //     this.setState({
-    //         location: 'test location'
-    //     })
-    // }
+  // componentDidMount() {
+  //     this.setState({
+  //         location: 'test location'
+  //     })
+  // }
 
-    onChangeLocation(e) {
-        this.setState({
-            location: e.target.value
-        });
-    } 
+  onChangeLocation(e) {
+    this.setState({
+      location: e.target.value,
+    });
+  }
 
-    onChangeLandmark(e) {
-        this.setState({
-            landmark: e.target.value
-        });
-    } 
+  onChangeLandmark(e) {
+    this.setState({
+      landmark: e.target.value,
+    });
+  }
 
-    onChangeViewpoint(e) {
-        this.setState({
-            viewpoint: e.target.value
-        });
-    } 
+  onChangeViewpoint(e) {
+    this.setState({
+      viewpoint: e.target.value,
+    });
+  }
 
-    onChangePhotospot(e) {
-        this.setState({
-            photospot: e.target.value
-        });
-    } 
+  onChangePhotospot(e) {
+    this.setState({
+      photospot: e.target.value,
+    });
+  }
 
-    onChangeCafe(e) {
-        this.setState({
-            cafe: e.target.value
-        });
-    } 
+  onChangeCafe(e) {
+    this.setState({
+      cafe: e.target.value,
+    });
+  }
 
-    onSubmit(e) {
-        e.preventDefault();
+  onSubmit(e) {
+    e.preventDefault();
 
-        const trip = {
-            location: this.state.location,
-            landmark: this.state.landmark,
-            viewpoint: this.state.viewpoint,
-            photospot: this.state.photospot,
-            cafe: this.state.cafe
-        }
+    const trip = {
+      location: this.state.location,
+      landmark: this.state.landmark,
+      viewpoint: this.state.viewpoint,
+      photospot: this.state.photospot,
+      cafe: this.state.cafe,
+    };
 
-        console.log(trip);
+    console.log(trip);
 
-        axios.post('http://localhost:3000/trip/add', trip)
-            .then(res => console.log(res.body));
+    axios
+      .post("https://my-travelplanner.herokuapp.com/trip/add", trip)
+      .then((res) => console.log(res.body));
 
-        // window.location = '/mytrips'
-    }
-    render() {
-        return (
-            <div>
-               <h3>Plan a new trip</h3>
-               <p>Add all the places you would like to visit during your trip and don't forget to add those import coffee stops in some local cafes</p>
-               <form onSubmit={this.onSubmit}>
-                   <div className="form-group">
-                       <label>Location: </label>
-                       <input type="text"
-                        required
-                        className="form-control"
-                        value={this.state.location}
-                        onChange={this.onChangeLocation}
-                        />
-                   </div>
-                   <div className="form-group">
-                       <label>Landmark: </label>
-                       <input type="text"
-                        className="form-control"
-                        value={this.state.landmark}
-                        onChange={this.onChangeLandmark}
-                        />
-                   </div>
-                   <div className="form-group">
-                       <label>Viewpoint: </label>
-                       <input type="text"
-                        className="form-control"
-                        value={this.state.viewpoint}
-                        onChange={this.onChangeViewpoint}
-                        />
-                   </div>
-                   <div className="form-group">
-                       <label>Photospot: </label>
-                       <input type="text"
-                        className="form-control"
-                        value={this.state.photospot}
-                        onChange={this.onChangePhotospot}
-                        />
-                   </div>
-                   <div className="form-group">
-                       <label>Cafe: </label>
-                       <input type="text"
-                        className="form-control"
-                        value={this.state.cafe}
-                        onChange={this.onChangeCafe}
-                        />
-                   </div>
+    // window.location = '/mytrips'
+  }
+  render() {
+    return (
+      <div>
+        <h3>Plan a new trip</h3>
+        <p>
+          Add all the places you would like to visit during your trip and don't
+          forget to add those import coffee stops in some local cafes
+        </p>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Location: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.location}
+              onChange={this.onChangeLocation}
+            />
+          </div>
+          <div className="form-group">
+            <label>Landmark: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.landmark}
+              onChange={this.onChangeLandmark}
+            />
+          </div>
+          <div className="form-group">
+            <label>Viewpoint: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.viewpoint}
+              onChange={this.onChangeViewpoint}
+            />
+          </div>
+          <div className="form-group">
+            <label>Photospot: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.photospot}
+              onChange={this.onChangePhotospot}
+            />
+          </div>
+          <div className="form-group">
+            <label>Cafe: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.cafe}
+              onChange={this.onChangeCafe}
+            />
+          </div>
 
-                   <div className="form-group">
-                       <input type="submit" value="Create Trip Plan" className="btn btn-primary" />
-                   </div>
-               </form>
-            </div>
-        )
-    }
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Trip Plan"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
