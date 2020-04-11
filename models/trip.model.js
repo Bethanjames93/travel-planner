9b5ebbf3ada50aba25d86be5f520b6591414fa7b
Tom Buckley <thomasbuckley@gmail.com>
2020-04-11 11:14:45 +0100

    Add serving routing and models

---
+++ models/trip.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const tripSchema = new Schema({
+    location: {type: String, required: true},
+    landmark: {type: String},
+    viewpoint: {type: String},
+    photospot: {type: String},
+    cafe: {type: String},
+}, {
+    timestamps: true,
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+module.exports = Trip;
