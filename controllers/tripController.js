8b751089d050feec4542ff60679c02add57ec350
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-04-16 21:52:09 +0100

    updated controller to find all

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -3,6 +3,6 @@ const db = require("../models");
 module.exports = {
     findAll: function(req, res) {
         console.log("This is my DB", db);
-        db.Trip.findOne({}).then(dbTrip => res.json(dbTrip)).catch(err => res.status(422).json(err));
+        db.Trip.find({}).then(dbTrip => res.json(dbTrip)).catch(err => res.status(422).json(err));
     },
 }
